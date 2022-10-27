2a3e89ce06462a7e748066d1038c2ea5133cc20c
00hello <445986+00hello@users.noreply.github.com>
2022-10-27 17:45:58 -0400

    changed WaveFunctionFunctionMaxBytes size

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -138,7 +138,7 @@ parameter_types! {
 	pub BlockLength: frame_system::limits::BlockLength = frame_system::limits::BlockLength
 		::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
 	pub const SS58Prefix: u8 = 42;
-	pub const WaveFunctionFunctionMaxBytes: u32 = 1556;
+	pub const WaveFunctionFunctionMaxBytes: u32 = 6000;
 }
 
 // Configure FRAME pallets to include in runtime.
